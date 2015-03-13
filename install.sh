10b4d964a141b6029121a7b2add727771b66eafb
Matthew Langbehn <matthew.langbehn@gmail.com>
2015-03-12 19:33:53 -0500

    Current work on installation script. Working to test for default shell, current shell, and if the suer wants to source the new zshrc...work in progress...

--- install.sh
+++ install.sh
@@ -19,9 +19,44 @@ if [ -f $HOME/.zshrc ]; then
     cp $HOME/.zshrc $HOME/.zshrc.bak
 fi
 curl https://raw.githubusercontent.com/mlangbehn/DotFiles/master/zshrc > $HOME/.zshrc
+
+# Check if zsh is the default shell
+#   If not, verify that it is installed
+#   If it is, offer to make it to the default
+
 echo
-echo "Changing default shell to zsh, and switching to it."
-echo
-chsh -s `which zsh`
-/usr/bin/env zsh
-. $HOME/.zshrc
+echo "Checking your default shell..."
+if [ "$SHELL" != /bin/zsh ]; then
+    CHANGE_DEFAULT_SHELL = yes
+    echo
+    echo "Your default shell is currently $SHELL."
+    echo "Would you like to change your default shell to zsh? (YES/no)"
+    read $CHANGE_DEFAULT_SHELL
+
+    echo $CHANGE_DEFAULT_SHELL
+    echo "$CHANGE_DEFAULT_SHELL"
+    if [ "$CHANGE_DEFAULT_SHELL" == yes ]; then
+        chsh -s `which zsh`
+    fi
+fi
+
+ps `echo $$` | grep zsh
+if [ $? = "0" ]; then
+    CHANGE_SHELL = yes
+    echo
+    echo "You are not currently running zsh."
+    echo "Would you like to change into zsh? (YES/no)"
+    read $CHANGE_SHELL
+
+    if [ $CHANGE_SHELL ]; then
+        /usr/bin/env zsh
+    fi
+else
+    SOURCE_ZSHRC
+    echo "Would you like to source your new ~/.zshrc? (YES/no)"
+    read $SOURCE_ZSHRC
+
+    if [ $SOURCE_ZSHRC ]; then
+        . $HOME/.zshrc
+    fi
+fi
